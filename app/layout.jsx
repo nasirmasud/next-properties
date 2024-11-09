eefc50292184b5a4d96d9dc86af31071d6ec4327
Nasir Masud <nasir.masud@ymail.com>
2024-11-09 22:22:39 +0600

    Added PropertyCard

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 export const metadata = {
@@ -14,6 +15,7 @@ const MainLayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
