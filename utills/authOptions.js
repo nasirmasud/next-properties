7b0c589725f206fd56f074136c8042614c1d3f24
Nasir Masud <nasir.masud@ymail.com>
2024-12-01 22:06:35 +0600

    Added Auth Provider

---
+++ utills/authOptions.js
@@ -0,0 +1,34 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    // Invoke on successful signin
+    async signIn({ profile }) {
+      // 1. Connect to database
+      // 2. Check if user exists
+      // 3. If not, then add user to database
+      // 4. Return true to allow sign in
+    },
+    // Modifies the session object
+    async session({ session }) {
+      // 1. Get user from database
+      // 2. Assign the user id to the session
+      // 3. Return session
+    },
+  },
+};
