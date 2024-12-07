779f092b1aa2c424ed04b6e03e1c8e48a224759d
Nasir Masud <nasir.masud@ymail.com>
2024-12-07 21:47:58 +0600

    Get the user and submitted to database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utills/getSessionUser";
 
 //GET /api/properties
 export const GET = async (request) => {
@@ -18,6 +19,15 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
   try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
     const formData = await request.formData();
     //access all values from amenities and images
     const amenities = formData.getAll("amenities");
@@ -49,14 +59,22 @@ export const POST = async (request) => {
         email: formData.get("seller_info.email"),
         phone: formData.get("seller_info.phone"),
       },
-      images,
+      owner: userId,
+      // images,
     };
 
-    console.log(propertyData);
+    // Save data to database
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
 
-    return new Response(JSON.stringify({ message: "Success" }), {
-      status: 200,
-    });
+    //redirect to property page after submit to database
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+    );
+
+    // return new Response(JSON.stringify({ message: "Success" }), {
+    //   status: 200,
+    // });
   } catch (error) {
     return new Response("Failed to add property", { status: 500 });
   }
