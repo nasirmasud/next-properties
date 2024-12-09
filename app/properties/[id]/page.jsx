693f19ac96a49419fc543becb5929bdfb18b1461
Nasir Masud <nasir.masud@ymail.com>
2024-12-09 11:36:11 +0600

    Added Property Image Display to Details Page

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -2,6 +2,7 @@
 
 import PropertyDetails from "@/components/PropertyDetails";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyImages from "@/components/PropertyImages";
 import Spinner from "@/components/Spinner";
 import { fetchProperty } from "@/utills/requests";
 import Link from "next/link";
@@ -156,6 +157,7 @@ const PropertyPage = () => {
               </div>
             </div>
           </section>
+          <PropertyImages images={property.images} />
         </>
       )}
     </>
