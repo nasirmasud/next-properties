f4e8cde329fdec902312094332c865902d3ed318
Nasir Masud <nasir.masud@ymail.com>
2024-12-09 21:14:31 +0600

    Added delete property button active

--- app/api/properties/[id]/route.js
+++ app/api/properties/[id]/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utills/getSessionUser";
 
 // GET /api/properties/:id
 export const GET = async (request, { params }) => {
@@ -17,6 +18,41 @@ export const GET = async (request, { params }) => {
 
     // Return the property data
     return new Response(JSON.stringify(property), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error fetching property:", error);
+    return new Response("Something Went Wrong", { status: 500 });
+  }
+};
+
+// DELETE /api/properties/:id
+export const DELETE = async (request, { params }) => {
+  try {
+    const propertyId = params.id;
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+    const { userId } = sessionUser;
+    // Connect to the database
+    await connectDB();
+
+    // Find the property by ID
+    const property = await Property.findById(propertyId);
+    if (!property) return new Response("Property Not Found", { status: 404 });
+
+    //verify the ownership
+    if (property.owner.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    await property.deleteOne();
+
+    // Return the property data
+    return new Response("Property Deleted", {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
