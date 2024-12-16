a553baa6925a4df3c6d81f9ab332907804a1c056
Nasir Masud <nasir.masud@ymail.com>
2024-12-16 19:05:30 +0600

    Display search results and added search box on properties page

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,7 +1,12 @@
 "use client";
 
+import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
+import Spinner from "@/components/Spinner";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const SearchResultsPage = () => {
   const searchParams = useSearchParams();
@@ -35,9 +40,39 @@ const SearchResultsPage = () => {
     fetchSearchResults();
   }, [location, propertyType]);
 
-  console.log(properties);
-
-  return <div>SearchResultsPage</div>;
+  return (
+    <>
+      <section className='bg-blue-700 py-4'>
+        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+          <PropertySearchForm />
+        </div>
+      </section>
+      {loading ? (
+        <Spinner loading={loading} />
+      ) : (
+        <section className='px-4 py-6'>
+          <div className='container-xl lg:container m-auto'>
+            <Link
+              href='/properties'
+              className='flex items-center text-blue-500 hover:underline mb-3'
+            >
+              <FaArrowAltCircleLeft className='mr-2 mb-1' /> Back to properties
+            </Link>
+            <h1 className='text-2xl mb-4'>Search Results</h1>
+            {properties.length === 0 ? (
+              <p>No Search results Found</p>
+            ) : (
+              <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+              </div>
+            )}
+          </div>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default SearchResultsPage;
